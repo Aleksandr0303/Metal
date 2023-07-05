77c1b6e0c636057b8a97cf8a2f45932e133f1698
Alesandr <golubalex800@gmail.com>
2023-07-05 19:29:21 +0300

    start PageSpeed

---
+++ dist/js/slider-setting.js
@@ -0,0 +1,116 @@
+document.addEventListener('DOMContentLoaded', function() {
+	//header
+	const hamburger = document.querySelector('.hamburger');
+	const header = document.querySelector('.header');
+	const close = document.querySelector('.close');
+	const tab = document.querySelector('#tab');
+	const tabActive = document.querySelector('.menu__metal_activ');
+	const wrapper = document.querySelector('.menu__list');
+
+	function navWindowClose() {
+		hamburger.style.display = 'block';
+		close.style.display = 'none';
+		header.classList.remove('header_activ');
+		const menuList = document.querySelector('.menu__list');
+		menuList.style.display = 'none';
+	}
+	hamburger.addEventListener('click', function() {
+		hamburger.style.display = 'none';
+		close.style.display = 'block';
+		header.classList.add('header_activ');
+		const headerActiv = document.querySelector('.header_activ');
+		headerActiv.style.display = 'block';
+		const menuList = document.querySelector('.menu__list');
+		menuList.style.display = 'flex';
+	});
+	close.addEventListener('click', navWindowClose);
+	
+	tab.addEventListener('click', function(event) {
+		event.preventDefault();
+		if (getComputedStyle(tabActive).display == 'none') {
+			tabActive.style.display = 'flex';
+		} else if (getComputedStyle(tabActive).display == 'flex') {
+			tabActive.style.display = 'none';
+		}
+	});
+
+	wrapper.addEventListener('click', function(event) {
+		if (event.target && event.target.tagName == 'A' && document.documentElement.clientWidth <= 767) {
+			navWindowClose();
+		}
+	});
+
+	//tabs-link
+	
+  	function simulateTab (links, cards) {
+		const catalogLinks = document.querySelectorAll(links);
+		const priceCards = document.querySelectorAll(cards);
+
+		function showCard () {
+			priceCards.forEach((card) => {
+				card.style.display = 'none';
+				const cardId = card.getAttribute('id');
+				const linkId = localStorage.getItem('save');
+				
+				if (cardId == linkId.replace(/\price.html#/, '')) {
+					card.style.display = 'block';
+				}
+			});
+		}
+	
+		catalogLinks.forEach((btn) => {
+			btn.addEventListener('click', (e) => {
+				lincId = e.target.getAttribute('href');
+				localStorage.setItem('save', lincId);
+
+				showCard ();
+			});
+		});
+	
+		showCard ();
+	}
+	simulateTab ('.btn_mod', '.price__card');
+	simulateTab ('.menu__metal > li > a', '.price__card');
+
+  $(document).ready(function(){
+    $('.slider__wrapper').slick({
+    autoplay:true,
+    autoplaySpeed:3000,
+    arrows:true,
+    prevArrow:$('.slider__prev'),
+    nextArrow:$('.slider__next'),
+    centerMode:true,
+    slidesToShow:5,
+    slidesToScroll:1,
+    responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 3, 
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 575,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+});
+
+});
+
